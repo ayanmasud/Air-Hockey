66043d42c2b778b898638d6b7bdcfa243524bb14
ayan masud <120285056+ayanmasud@users.noreply.github.com>
2024-05-19 23:13:11 -0700

    Update Table.java

--- Table.java
+++ Table.java
@@ -2,17 +2,25 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 
-public class Table extends JPanel {
+public class Table extends JPanel implements Runnable {
 
-    int playerMalletX;
-    int playerMalletY;
+    int playerMalletX = 250;
+    int playerMalletY = 800;
+    float playerMalletSpeed = 0;
+    Puck puck;
 
     public Table() {
         super();
+        puck = new Puck(this);
         addMouseMotionListener(new MouseAdapter() {
             public void mouseMoved(MouseEvent e) {
                 playerMalletX = e.getX();
                 playerMalletY = e.getY();
+
+                if(PlayerHitPuck()) {
+                    puck.move();
+                }
+
                 if(playerMalletY < 470) { // restrictions to ensure players mallet doesn't go off the table
                     playerMalletY = 470;
                 }
@@ -25,10 +33,37 @@ public class Table extends JPanel {
                 if(playerMalletX > 454) {
                     playerMalletX = 454;
                 }
-                System.out.println(playerMalletX + "," + playerMalletY);
-                repaint(); // Request a repaint whenever mouse moves
+                //System.out.println(playerMalletX + "," + playerMalletY);
+                repaint();
             }
         });
+        Thread playerMalletThread = new Thread(this);
+        playerMalletThread.start();
+    }
+
+    public boolean PlayerHitPuck() {
+        double distance = Math.sqrt(Math.pow(playerMalletX - puck.puckX, 2) + Math.pow(playerMalletY - puck.puckY, 2));
+        if(distance <= 55 && playerMalletY > 440) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void run() { // used for determining how fast the player is moving
+        while (true) {
+            int posx = playerMalletX;
+            int posy = playerMalletY;
+            try {
+                Thread.sleep(20);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            int posxChange = Math.abs(playerMalletX-posx);
+            int posyChange = Math.abs(playerMalletY-posy);
+            double distance = Math.sqrt(Math.pow(posxChange, 2) + Math.pow(posyChange, 2)); // compares current position from position 20 millis ago
+            playerMalletSpeed = (float)(distance);
+        }
     }
 
     public void paintComponent(Graphics g) {
@@ -42,6 +77,7 @@ public class Table extends JPanel {
         g.fillRect((500/7)*2,883,(500/7)*3,10);
         g.setColor(Color.RED); // Set color to red
         g.fillOval(playerMalletX - 30, playerMalletY - 30, 60, 60); // Draw mallet centered at mouse position
+        puck.draw(g); // Draw the puck
     }
 
 }
